4d5ad606249132540754ff9971966e01fd90cc1a
Home <renddi@yahoo.com>
2018-09-21 20:46:42 +0400

    asdfgh

--- src/Main.java
+++ src/Main.java
@@ -9,45 +9,62 @@ public class Main {
     public static void main(String[] args) {
 
 ////        ArrayList
-//        ArrayList<String> arrList = new ArrayList();
-//        arrList.add("Added String");
-//        arrList.add(1, "String element");
-//        arrList.get(0);
-//        arrList.set(0, "Changed String");
-//        arrList.remove(0);
-//        arrList.remove("Changed String");
-////        LinkedList
-//        LinkedList<Integer> linkedList = new LinkedList();
-//        linkedList.add(2); // add by element
-//        linkedList.add(1, 2);
+        ArrayList<String> arrList = new ArrayList();
+        arrList.add("Added String");
+        arrList.add(1, "String element");
+        arrList.get(0);
+        arrList.set(0, "Changed String");
+        arrList.remove(0);
+        arrList.remove("Changed String");
+        //       LinkedList
+       LinkedList<Integer> linkedList = new LinkedList();
+        //       linkedList .add(2);
+//     linkedList.add(1, 2);
 //        linkedList.get(0); // get by Index
 //        linkedList.getFirst();
 //        linkedList.getLast();
-//        linkedList.set(1, 123); // set by Index element
+//      linkedList.set(1, 123); // set by Index element
 //        linkedList.remove();
-//        Integer dellInteger = 123;
-//        boolean tmp = linkedList.remove(dellInteger);
-//        Integer tmp2 = linkedList.remove(2);
+        Integer dellInteger = 123;
+        boolean tmp = linkedList.remove(dellInteger);
+//       Integer tmp2 = linkedList.remove(2);
 //        linkedList.removeFirst();
 //        linkedList.removeLast();
 //        Set
-//        HashSet<Long> setList = new HashSet();
-//        setList.add(321L);
-//        setList.add(3221L);
-//        setList.add(3231L);
-//        setList.add(321L);
-//        Iterator iterator = setList.iterator();
-//        while (iterator.hasNext()){
-//            System.out.println(iterator.next());
-//        }
+        HashSet<Long> setList = new HashSet();
+        setList.add(321L);
+        setList.add(32221L);
+       setList.add(322231L);
+        setList.add(321111L);
+        Iterator iterator = setList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
 //Map
-        Map map = new HashMap<String, Integer>();
-        map.put("one", 1);
+        Map map = new HashMap<String, List<Integer>>();
+
+        map.put("two", 1);
         map.put("two", 2);
         map.put("two", 3);
 
+        String key = ("one");
+
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+      //      System.out.println ("1,2,3,4,5,6") ;
+
+        }
+        map.get(key);
+
+   //     map.remove(key = "one");
+    //    Set keys = map.keySet();
+
         for (Object value : map.values()) {
             System.out.println(value);
+            System.out.println ("1,2,3,4,5,6") ;
+
         }
 
     }
